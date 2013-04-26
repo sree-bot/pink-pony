4ff06bee050603231b708826e851f7820641e24c
Sreekanth Ramakrishnan <sreekanth@fake.server>
2013-04-26 17:03:15 +0530

    Fixing the models to form proper hirearchy

--- app/models.py
+++ app/models.py
@@ -6,44 +6,48 @@ class Quiz(db.Model):
     """
     Model Class representing the quiz object.
     """
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    flavor_text = db.Column(db.Text)
-    created_date = db.Column(db.DateTime)
-    publish_date = db.Column(db.DateTime)
-    no_of_rounds = db.Column(db.Integer)
-    #rounds = db.relationship('QuizRound', backref = 'quiz', lazy = 'dynamic')
+    flavor_text = db.Column(db.Text, default="")
+    created_date = db.Column(db.DateTime, default=datetime.utcnow())
+    publish_date = db.Column(db.DateTime, default=datetime.utcnow())
+    rounds = db.relationship('Round', cascade='all, delete',
+                             backref='quiz', lazy='dynamic')
     
     def __repr__(self):
-        return '<Quiz %r , created on %r to be published on %r with %r rounds>' % (title, created_date, publish_date, rounds)
+        return '<Quiz %r>' % (self.title)
 
 
 class Round(db.Model):
     """
     Each round of the Quiz
     """
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'))
     title = db.Column(db.String(256))
-    no_of_questions = db.Column(db.Integer, default = 1)
-    decay = db.Column(db.Integer, default = 1)
-    max_score = db.Column(db.Integer)
-    
+    decay = db.Column(db.Integer, default=1)
+    max_score = db.Column(db.Integer, default=1)
+    questions = db.relationship('Question', cascade='all, delete',
+                                backref='round', lazy='dynamic')
+
     def __repr__(self):
-        return '<QuizRound title %r no_of_questions %r decay rate %r max_score_per_question %r>' % (title, no_of_question, decay, max_score)
+        return '<QuizRound %r>' % (self.title)
 
 
-class Question(db.Model):
+class Question(db.Model):   
     """
     Each question in the the round of quiz
     """
     
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     question_text = db.Column(db.Text)
     answer_text = db.Column(db.Text)
-    max_score = db.Column(db.Integer)
-    decay = db.Column(db.Integer)
+    max_score = db.Column(db.Integer, default=1)
+    decay = db.Column(db.Integer, default=1)
     round_id = db.Column(db.Integer, db.ForeignKey('round.id'))
     
     def __repr__(self):
-        return '<Question %r %r %r %r >' % (question_text, answer_text, max_score, decay)
+        return '<Question %r %r %r %r >' % (self.question_text,
+                                                self.answer_text,
+                                                self.max_score,
+                                                self.decay)
